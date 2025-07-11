fba64971dd2d00d902d4372238cbb806e92a4f9c
TakehiroShinzawa <t_shinz@a.toshima.ne.jp>
2025-07-11 15:09:48 +0900

    Initial commit for ResourceMaker

---
+++ Package.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace ResourceMaker
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(Package.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class Package : AsyncPackage
+    {
+        /// <summary>
+        /// Package GUID string.
+        /// </summary>
+        public const string PackageGuidString = "0bc0d3e7-132c-42da-9df0-b0641d1699a0";
+
+        private static readonly Guid PackageGuid = new Guid("0bc0d3e7-132c-42da-9df0-b0641d1699a0");
+        private static readonly Guid CommandSetGuid = new Guid("1c689cc2-75d2-4e55-a12b-2d7d30c2c8e3"); // メニュー用GUID
+        private const int ResourceizeCommandId = 0x0100;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Package"/> class.
+        /// </summary>
+        public Package()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            OleMenuCommandService commandService = await GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var commandId = new CommandID(CommandSetGuid, ResourceizeCommandId);
+                var menuItem = new MenuCommand(ExecuteResourceize, commandId);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        private void ExecuteResourceize(object sender, EventArgs e)
+        {
+            VsShellUtilities.ShowMessageBox(
+                this,
+                "XAMLリソース化コマンドが実行されました！",
+                "ResourceMaker",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+        #endregion
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# ResourceMaker
+
+ResourceMaker は、Visual Studio 拡張や WinUI アプリケーション向けに設計されたリソース管理支援ライブラリです。  
+
+## 🔧 主な機能
+
+
+## 🧩 利用方法
+
